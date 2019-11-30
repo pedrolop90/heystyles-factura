e1f84778f388998107100a920278bf6444c9fe6f
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2019-11-30 15:24:21 -0500

    saber si un abono, ha dejado la factura completa...

--- factura-api/src/main/java/com/heystyles/factura/api/message/MessageKeys.java
+++ factura-api/src/main/java/com/heystyles/factura/api/message/MessageKeys.java
@@ -8,4 +8,6 @@ public interface MessageKeys extends com.heystyles.common.util.MessageKeys {
 
     String ABONO_NOT_FOUND = "heystyles.factura.abono.not-found";
 
+    String ABONO_MAYOR_FACTURA_VALOR = "heystyles.factura.abono-valor.factura-valor";
+
 }
---
+++ factura-api/src/main/java/com/heystyles/factura/api/validator/AbonoAcumuladoFacturaValidator.java
@@ -0,0 +1,48 @@
+package com.heystyles.factura.api.validator;
+
+import com.heystyles.common.validation.ValidationError;
+import com.heystyles.common.validation.Validator;
+import com.heystyles.common.validation.groups.Insert;
+import com.heystyles.common.validation.groups.Update;
+import com.heystyles.factura.api.message.MessageKeys;
+import com.heystyles.factura.api.service.FacturaService;
+import com.heystyles.factura.core.domain.Abono;
+import com.heystyles.factura.core.domain.Factura;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.context.i18n.LocaleContextHolder.getLocale;
+
+@Component
+public class AbonoAcumuladoFacturaValidator implements Validator<Abono> {
+
+    @Autowired
+    private FacturaService facturaService;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    public List<ValidationError> validate(Abono abono) {
+        List<ValidationError> errors = new ArrayList<>();
+        Factura factura = facturaService.getFactura(abono.getFacturaId());
+        if (abono.getValor() > factura.getValorTotal()) {
+            errors.add(new ValidationError(
+                    "valor",
+                    messageSource.getMessage(MessageKeys.ABONO_MAYOR_FACTURA_VALOR,
+                            null, getLocale())
+            ));
+        }
+        return errors;
+    }
+
+    @Override
+    public List<Class<?>> getScopes() {
+        return Arrays.asList(Insert.class, Update.class);
+    }
+}
